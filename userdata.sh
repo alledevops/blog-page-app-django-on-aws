d40b2f658c2156238f5f08eabc7f340ff3d48fc2
mustafag <gonenmustafam@gmail.com>
2023-10-25 18:55:50 +0300

    created userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get update -y
+apt-get install git -y
+apt-get install python3 -y
+cd /home/ubuntu/
+TOKEN="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"
+git clone https://$TOKEN@github.com/alledevops/blog-page-app-django-on-aws
+cd /home/ubuntu/blog-page-app-django-on-aws
+apt install python3-pip -y
+apt-get install python3.7-dev default-libmysqlclient-dev -y
+pip3 install -r requirements.txt
+cd /home/ubuntu/blog-page-app-django-on-aws/src
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:80
