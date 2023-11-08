e4161956131b0b13281eaebca07c883230464e9b
mustafag <gonenmustafam@gmail.com>
2023-11-08 14:42:51 +0300

    updated userdata

--- userdata.sh
+++ userdata.sh
@@ -3,14 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
+TOKEN="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"
 git clone https://$TOKEN@github.com/alledevops/blog-page-app-django-on-aws
 cd /home/ubuntu/blog-page-app-django-on-aws
 apt-get install python3-pip -y
 apt-get install python3.8-dev default-libmysqlclient-dev -y
 apt-get install libjpeg-dev -y
 pip install testresources
-pip install --upgrade pip
 pip3 install -r requirements.txt
 cd /home/ubuntu/blog-page-app-django-on-aws/src
 python3 manage.py collectstatic --noinput
