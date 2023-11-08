aee9e2793c83cc4d29bee77d4629ee3cad852cf4
mustafag <gonenmustafam@gmail.com>
2023-11-08 14:49:44 +0300

    change of password and rds endpoint

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -143,7 +143,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'awscapstonesmustafablog' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'awscapstonemustafablog' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
